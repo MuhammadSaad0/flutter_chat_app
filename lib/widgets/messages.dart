e32813ffc247289695749cedbb618eeeae453d5b
unknown <muhammadsaadz100@gmail.com>
2022-06-29 14:57:42 +0500

    Added option to send images

--- lib/widgets/messages.dart
+++ lib/widgets/messages.dart
@@ -31,70 +31,97 @@ class _MessagesState extends State<Messages> {
 
         return ListView.builder(
           reverse: true,
-          itemBuilder: (ctx, index) =>
-              Stack(clipBehavior: Clip.none, children: [
-            Bubble(
-              key: ValueKey(chatDocs[index].id),
-              stick: false,
-              padding: BubbleEdges.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    chatDocs[index]['username'],
-                    style: TextStyle(
-                      fontSize: 11,
-                      color: Color.fromRGBO(255, 255, 255, 0.75),
-                    ),
-                    textAlign: chatDocs[index]['userId'] ==
-                            FirebaseAuth.instance.currentUser.uid
-                        ? TextAlign.end
-                        : TextAlign.start,
+          itemBuilder: (ctx, index) => Bubble(
+            key: ValueKey(chatDocs[index].id),
+            stick: false,
+            padding: BubbleEdges.all(10),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                if (chatDocs[index]['imageUrl'] != null)
+                  Image(
+                    frameBuilder:
+                        (context, child, frame, wasSynchronouslyLoaded) {
+                      return child;
+                    },
+                    loadingBuilder: (context, child, loadingProgress) {
+                      if (loadingProgress == null) {
+                        return child;
+                      } else {
+                        return CircularProgressIndicator();
+                      }
+                    },
+                    height: 200,
+                    image: NetworkImage(chatDocs[index]['imageUrl']),
                   ),
-                  Text(
+                if (chatDocs[index]['imageUrl'] != null)
+                  SizedBox(
+                    height: 10,
+                  ),
+                Text(
+                  chatDocs[index]['username'],
+                  style: TextStyle(
+                    fontSize: 11,
+                    color: Color.fromRGBO(255, 255, 255, 0.75),
+                  ),
+                  textAlign: chatDocs[index]['userId'] ==
+                          FirebaseAuth.instance.currentUser.uid
+                      ? TextAlign.end
+                      : TextAlign.start,
+                ),
+                SizedBox(
+                  height: 2,
+                ),
+                Container(
+                  width: (chatDocs[index]['imageUrl'] != null ? 165 : null),
+                  child: Text(
                     chatDocs[index]['text'],
-                    style: TextStyle(fontSize: 18),
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
                   ),
-                ],
-              ),
-              alignment: chatDocs[index]['userId'] ==
-                      FirebaseAuth.instance.currentUser.uid
-                  ? Alignment.topRight
-                  : Alignment.topLeft,
-              nip: chatDocs[index]['userId'] ==
-                      FirebaseAuth.instance.currentUser.uid
-                  ? BubbleNip.rightTop
-                  : BubbleNip.leftTop,
-              color: chatDocs[index]['userId'] ==
-                      FirebaseAuth.instance.currentUser.uid
-                  ? Colors.pink
-                  : Colors.blue,
-              margin: BubbleEdges.symmetric(vertical: 14, horizontal: 5),
-              elevation: 10,
+                ),
+              ],
             ),
-            Positioned(
-                top: -10,
-                left: chatDocs[index]['userId'] ==
-                        FirebaseAuth.instance.currentUser.uid
-                    ? MediaQuery.of(context).size.width /
-                        (MediaQuery.of(context).orientation ==
-                                Orientation.landscape
-                            ? 1.05
-                            : 1.1)
-                    : null,
-                right: chatDocs[index]['userId'] ==
-                        FirebaseAuth.instance.currentUser.uid
-                    ? null
-                    : MediaQuery.of(context).size.width /
-                        (MediaQuery.of(context).orientation ==
-                                Orientation.landscape
-                            ? 1.05
-                            : 1.1),
-                child: CircleAvatar(
-                  maxRadius: 15,
-                  backgroundImage: NetworkImage(chatDocs[index]['userImage']),
-                )),
-          ]),
+            alignment: chatDocs[index]['userId'] ==
+                    FirebaseAuth.instance.currentUser.uid
+                ? Alignment.topRight
+                : Alignment.topLeft,
+            nip: chatDocs[index]['userId'] ==
+                    FirebaseAuth.instance.currentUser.uid
+                ? BubbleNip.rightTop
+                : BubbleNip.leftTop,
+            color: chatDocs[index]['userId'] ==
+                    FirebaseAuth.instance.currentUser.uid
+                ? Colors.pink
+                : Colors.blue,
+            margin: BubbleEdges.symmetric(vertical: 14, horizontal: 5),
+            elevation: 10,
+          ),
+          // Positioned(
+          //     top: -10,
+          //     left: chatDocs[index]['userId'] ==
+          //             FirebaseAuth.instance.currentUser.uid
+          //         ? MediaQuery.of(context).size.width /
+          //             (MediaQuery.of(context).orientation ==
+          //                     Orientation.landscape
+          //                 ? 1.05
+          //                 : 1.1)
+          //         : null,
+          //     right: chatDocs[index]['userId'] ==
+          //             FirebaseAuth.instance.currentUser.uid
+          //         ? null
+          //         : MediaQuery.of(context).size.width /
+          //             (MediaQuery.of(context).orientation ==
+          //                     Orientation.landscape
+          //                 ? 1.05
+          //                 : 1.1),
+          //     child: CircleAvatar(
+          //       maxRadius: 15,
+          //       backgroundImage: NetworkImage(chatDocs[index]['userImage']),
+          //     )),
+
           itemCount: chatDocs.length,
         );
       },
--- lib/widgets/new_message.dart
+++ lib/widgets/new_message.dart
@@ -1,8 +1,11 @@
+import 'dart:async';
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:random_string_generator/random_string_generator.dart';
 
 class NewMessage extends StatefulWidget {
   const NewMessage({Key key}) : super(key: key);
@@ -14,12 +17,22 @@ class NewMessage extends StatefulWidget {
 class _NewMessageState extends State<NewMessage> {
   var _enteredMessage = "";
   File _pickedImage;
+  var generator = RandomStringGenerator(
+    hasAlpha: true,
+    alphaCase: AlphaCase.UPPERCASE_ONLY,
+    hasDigits: true,
+    hasSymbols: false,
+    minLength: 15,
+    maxLength: 25,
+    mustHaveAtLeastOneOfEach: true,
+  );
   var imagePicked = false;
+  var waiting = false;
   void _pickImage() async {
     final imagePicker = ImagePicker();
     final pickedImageFile = await imagePicker.pickImage(
       source: ImageSource.gallery,
-      imageQuality: 80,
+      imageQuality: 30,
     );
     setState(() {
       _pickedImage = File(pickedImageFile.path);
@@ -28,20 +41,65 @@ class _NewMessageState extends State<NewMessage> {
   }
 
   final _controller = new TextEditingController();
+  var url = null;
+  void handleTimeout() {
+    setState(() {
+      waiting = false;
+    });
+  }
+
+  Timer scheduleTimeout([int milliseconds = 10000]) =>
+      Timer(Duration(milliseconds: milliseconds), handleTimeout);
+
   void _sendMessage() async {
+    setState(() {
+      waiting = true;
+    });
     FocusScope.of(context).unfocus();
     final uId = await FirebaseAuth.instance.currentUser.uid;
     final userData =
         await FirebaseFirestore.instance.collection('users').doc(uId).get();
-    FirebaseFirestore.instance.collection('chat').add({
-      'text': _enteredMessage,
-      'createdAt': Timestamp.now(),
-      'userId': FirebaseAuth.instance.currentUser.uid,
-      'username': userData['username'],
-      'userImage': userData['imageUrl'],
+    if (_pickedImage != null) {
+      final reference = FirebaseStorage.instance
+          .ref()
+          .child('message_image')
+          .child(generator.generate() + ".jpg");
+      UploadTask uploadTask = reference.putFile(_pickedImage);
+      _controller.clear();
+
+      setState(() {
+        imagePicked = false;
+        //_pickedImage = null;
+        url = null;
+      });
+      uploadTask.whenComplete(() async {
+        url = await reference.getDownloadURL();
+        FirebaseFirestore.instance.collection('chat').add({
+          'text': _enteredMessage,
+          'createdAt': Timestamp.now(),
+          'userId': FirebaseAuth.instance.currentUser.uid,
+          'username': userData['username'],
+          //'userImage': userData['imageUrl'],
+          'imageUrl': url,
+        });
+      });
+    } else {
+      _controller.clear();
+      url = null;
+      FirebaseFirestore.instance.collection('chat').add({
+        'text': _enteredMessage,
+        'createdAt': Timestamp.now(),
+        'userId': FirebaseAuth.instance.currentUser.uid,
+        'username': userData['username'],
+        //'userImage': userData['imageUrl'],
+        'imageUrl': url,
+      });
+    }
+    setState(() {
+      //waiting = false;
+      scheduleTimeout(5 * 1000);
+      url = null;
     });
-    _controller.clear();
-    _enteredMessage = "";
   }
 
   @override
@@ -74,11 +132,13 @@ class _NewMessageState extends State<NewMessage> {
                 image: FileImage(_pickedImage),
               )),
         IconButton(onPressed: _pickImage, icon: Icon(Icons.attachment)),
-        IconButton(
-          onPressed: _controller.text == "" ? null : _sendMessage,
-          icon: Icon(Icons.send),
-          color: Colors.pink,
-        )
+        if (!waiting)
+          IconButton(
+            onPressed: _controller.text == "" ? null : _sendMessage,
+            icon: Icon(Icons.send),
+            color: Colors.pink,
+          ),
+        if (waiting) CircularProgressIndicator(),
       ]),
     );
   }
